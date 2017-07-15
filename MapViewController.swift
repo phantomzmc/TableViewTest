9fd12e2ce4febc30f7ed9fc557658dd345d8cedc
Thunnathorn <phantomzmc@gmail.com>
2017-07-15 15:57:57 +0700

    mapkit and location

--- MapViewController.swift
+++ MapViewController.swift
@@ -8,17 +8,85 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController{
+class MapViewController: UIViewController,CLLocationManagerDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager : CLLocationManager!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        if (CLLocationManager.locationServicesEnabled() && CLLocationManager.authorizationStatus() == .authorizedAlways)
+        {
+            locationManager.requestAlwaysAuthorization()
+            locationManager.startUpdatingLocation()
+        }else{
+            locationManager.requestAlwaysAuthorization()
+            
+            let alertController = UIAlertController (title: "You allow use your GPS", message: "Please go to Setting", preferredStyle: .alert)
+            let settingsAction = UIAlertAction(title: "Settings", style: .default) { (_) -> Void in
+                guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
+                    return
+                }
+                
+                if UIApplication.shared.canOpenURL(settingsUrl) {
+                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                        print("Settings opened: \(success)") // Prints true
+                    })
+                }
+            }
+            alertController.addAction(settingsAction)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+            alertController.addAction(cancelAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }
 
-        // Do any additional setup after loading the view.
     }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        
+        let location = locations.last! as CLLocation
+        
+        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        self.mapView.setRegion(region, animated: true)
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            // If status has not yet been determied, ask for authorization
+            manager.requestAlwaysAuthorization()
+            break
+        case .authorizedWhenInUse:
+            // If authorized when in use
+            manager.startUpdatingLocation()
+            break
+        case .authorizedAlways:
+            // If always authorized
+            manager.startUpdatingLocation()
+            break
+        case .restricted:
+            // If restricted by e.g. parental controls. User can't enable Location Services
+            break
+        case .denied:
+            // If user denied your app access to Location Services, but can grant access from Settings.app
+            break
+        default:
+            break
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
